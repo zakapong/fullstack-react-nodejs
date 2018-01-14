e5218fec7d970f95b3af89a5dfb7279ee14b30e2
Zakaria Mahmud <zaka.pong@yahoo.com>
2018-01-14 12:04:12 +0100

    adding the backend with nodejs

--- fullstack-react/src/index.js
+++ fullstack-react/src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import 'semantic-ui-css/semantic.min.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./rootReducer";
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render( 
     <BrowserRouter>
+      <Provider store={store}>
     <App />
+    </Provider>
    </BrowserRouter>,  
     
 document.getElementById('root')
